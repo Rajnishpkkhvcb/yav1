b2deeb02faa5ba7ee1ecffc1416be22b200d2d55
Rajnishpkkhvcb <60421387+Rajnishpkkhvcb@users.noreply.github.com>
2021-04-19 17:59:31 +0530

    min and max values

--- BST.js
+++ BST.js
@@ -35,3 +35,34 @@ class BinarySearchTree {
   
   }
 }
+
+
+//min 
+
+getMinVal() {
+   if (this.root === null) {
+      throw "Empty tree!";
+   }
+   let currNode = this.root;
+
+   while (currNode.left !== null) {
+      currNode = currNode.left;
+   }
+   return currNode.data;
+}
+
+
+//max
+
+
+getMaxVal() {
+if (this.root === null) {
+      throw "Empty tree!";
+}
+let currNode = this.root;
+
+while (currNode.right !== null) {
+   currNode = currNode.right;
+}
+   return currNode.data;
+}
