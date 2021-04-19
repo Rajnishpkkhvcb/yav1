d0a37256dd4a440f0cd72910f25b27ef7268a132
Rajnishpkkhvcb <60421387+Rajnishpkkhvcb@users.noreply.github.com>
2021-04-19 15:10:36 +0530

    deleting user data

--- reg.js
+++ reg.js
@@ -1,4 +1,20 @@
+
 const data=axios.get(
 
-	"https://crudcrud.com/Dashboard/a071b85353444fca8e9764cd0c2f5bcf"
+	"https://crudcrud.com/api/a071b85353444fca8e9764cd0c2f5bcf"
 )
+.then(data=>console.log(data))
+
+const pt=axios.post(
+	"https://crudcrud.com/api/071b85353444fca8e9764cd0c2f5bcf"
+	{name:"raj"}
+	).then(res => showOutput(res))
+    .catch(err => console.error(err));
+
+
+function delete() {
+  axios
+    .delete('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res => showOutput(res))
+    .catch(err => console.error(err));
+}
